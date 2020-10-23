02e26ca3b0bd3e78faca06af764e96b8385d1650
dipzcoded <dipoakerele@gmail.com>
2020-10-23 23:04:45 +0100

    dd

--- app.js
+++ app.js
@@ -14,48 +14,48 @@ const path = require('path');
 
 // setting up middleware
 
-// set Security HTTP Headers
-app.use(helmet());
+// // set Security HTTP Headers
+// app.use(helmet());
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
+// if (process.env.NODE_ENV === "development") {
+//   app.use(morgan("dev"));
+// }
 
-// Limit Request from same API
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many request from this IP, please try again in an hour!",
-});
+// // Limit Request from same API
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: "Too many request from this IP, please try again in an hour!",
+// });
 
-app.use("/api", limiter);
+// app.use("/api", limiter);
 
-// Body parser, reading data from body into req.body
-app.use(
-  express.json({
-    limit: "10kb",
-  })
-);
+// // Body parser, reading data from body into req.body
+// app.use(
+//   express.json({
+//     limit: "10kb",
+//   })
+// );
 
-// Data sanitization against NoSQL query injection
-app.use(mongoSanitize());
+// // Data sanitization against NoSQL query injection
+// app.use(mongoSanitize());
 
-// Data sanitiation againt XSS
-app.use(xss());
+// // Data sanitiation againt XSS
+// app.use(xss());
 
-// Prevent parameter pollution
-app.use(
-  hpp({
-    whitelist: [
-      "duration",
-      "ratingsAverage",
-      "ratingsQuantity",
-      "maxGroupSize",
-      "difficulty",
-      "price",
-    ],
-  })
-);
+// // Prevent parameter pollution
+// app.use(
+//   hpp({
+//     whitelist: [
+//       "duration",
+//       "ratingsAverage",
+//       "ratingsQuantity",
+//       "maxGroupSize",
+//       "difficulty",
+//       "price",
+//     ],
+//   })
+// );
 
 
 
