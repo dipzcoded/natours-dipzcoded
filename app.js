46db6b4cb8c0b56aa3ac327d02b86165440bf3fe
dipzcoded <dipoakerele@gmail.com>
2020-10-23 21:01:40 +0100

    a

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ connectDB();
 
 // setting up middleware
 // serve static assets in production
+if(process.env.NODE_ENV === "production")
+{
+  // set static folder
+  app.use(express.static('view/build'));
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'view','build','index.html'))
+  })
+}
 
 
 // set Security HTTP Headers
@@ -76,15 +84,6 @@ app.use("/api/v1/auth", require(`${__dirname}/route/api/auth.js`));
 app.use("/api/v1/reviews", require(`${__dirname}/route/api/reviews`));
 app.use("/api/v1/booking", require(`${__dirname}/route/api/bookings`));
 app.use(ErrorHandlers);
-
-if(process.env.NODE_ENV === "production")
-{
-  // set static folder
-  app.use(express.static('view/build'));
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname,'view','build','index.html'))
-  })
-}
 
 // app.all("*", (req, res, next) => {
 //   next(new ApiError(`Route not found ${req.originalUrl}`, 404));
