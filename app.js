4c8fb0a1a621a1be89b35b5ec0cb457a5f068d65
dipzcoded <dipoakerele@gmail.com>
2020-10-23 20:45:51 +0100

    damn

--- app.js
+++ app.js
@@ -73,7 +73,6 @@ app.use("/api/v1/auth", require(`${__dirname}/route/api/auth.js`));
 app.use("/api/v1/reviews", require(`${__dirname}/route/api/reviews`));
 app.use("/api/v1/booking", require(`${__dirname}/route/api/bookings`));
 
-app.use(ErrorHandlers);
 if(process.env.NODE_ENV === "production")
 {
   // set static folder
@@ -82,6 +81,9 @@ if(process.env.NODE_ENV === "production")
     res.sendFile(path.resolve(__dirname,'view','build','index.html'))
   })
 }
+
+
+app.use(ErrorHandlers);
 
 app.all("*", (req, res, next) => {
   next(new ApiError(`Route not found ${req.originalUrl}`, 404));
