6ff3de3aa111cf6977a050fa7d8c29804b16590a
dipzcoded <dipoakerele@gmail.com>
2020-10-28 23:41:44 +0100

    working on webhooks

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -72,7 +72,7 @@ try {
 
     const tour = session.client_reference_id;
     const  user = (await User.findOne({email : session.customer_email})).id;
-    const price = session.amout_total / 100;
+    const price = session.amount_total / 100;
 
     await Booking.create({
         tour,
